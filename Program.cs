2bec08b63e35c7e16494fe354b49cfdfd49a8e79
Matthew Cataldi <mcataloe@gmail.com>
2017-11-15 12:42:57 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace new_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
